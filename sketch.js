8fcc0ae87b481d2a396108db5745c2bdda05fa98
tanaya <tanayar.kulkarni.sod24@anu.edu.in>
2025-09-16 12:37:16 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,60 @@
+function setup() {
+  createCanvas(500, 500);
+
+  background("black");
+
+  fill ("white")
+  square(290, 150, 55);
+  
+   fill ("white")
+  square(235, 150, 55);
+  
+   fill ("white")
+  square(180, 150, 55);
+  
+  fill ("white")
+  square(180, 205, 55);
+  
+  fill ("white")
+  square(235, 205, 55);
+  
+  fill ("white")
+  square(290, 205, 55);
+  
+   fill ("white")
+  square(290, 260, 55);
+  
+  fill ("white")
+  square(235, 260, 55);
+  
+  fill ("white")
+  square(180, 260, 55);
+  
+  fill ("white")
+  square(150, 315, 55);
+  
+  fill ("white")
+  square(205, 315, 55);
+  
+    fill ("white")
+  square(260, 315, 55);
+  
+   fill ("white")
+  square(315, 315, 55);
+  
+  fill ("black")
+  circle(235, 205, 20)
+  
+   fill ("black")
+  circle(290, 205, 20)
+  
+  
+  
+  
+  
+  
+  
+  
+
+  describe('A white square with a black outline in on a gray canvas.');
+}
